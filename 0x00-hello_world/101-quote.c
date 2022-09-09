aa9f9acc65fd441541d449ac5ff3c6bfc000822b
WhalesDbayor <wabalonline01@gmail.com>
2022-09-08 17:54:11 -0700

    Added quote message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - print quote
+ *
+ * Description: prints Dora Korpar's quote
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
