7e3cdba2703ed13be0fc74619767f2d22b4af3cd
WhalesDbayor <wabalonline01@gmail.com>
2022-09-08 17:19:29 -0700

    New fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,11 @@
  */
 int main(void)
 {
-	puts("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	puts("Size of an int: %d byte(s)\n", (int) sizeof(int));
-	puts("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-	puts("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-	puts("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+
 	return (0);
 }
